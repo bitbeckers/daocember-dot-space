33a323980cb27d7f86fcfca7687a7effb0f94ada
bitbeckers <code@bitbeckers.com>
2023-11-22 10:24:07 +0100

    fix(trol): trol page

---
+++ app/trololol/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Logo from "../components/Logo";
+
+export default function Trololol() {
+  return (
+    <main className="flex flex-col min-h-screen min-w-screen bg-three-wise-summoners bg-cover text-white justify-center">
+      <Logo />
+    </main>
+  );
+}
