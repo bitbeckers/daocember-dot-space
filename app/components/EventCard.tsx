49586f4c868edef9eb3ee7f10096643e7c4c57ea
Aciid0 <aciid0@>
2023-12-01 19:51:16 +0100

    Refined when to show Join - 30mins before and keep for 30 mins after

--- app/components/EventCard.tsx
+++ app/components/EventCard.tsx
@@ -74,12 +74,14 @@ const EventCard = ({
         </p>
         <div className="flex justify-between">
           <p className="text-gray-400 text-base">{eventTypes[type].title}</p>
-          {start.diffNow("minutes").minutes < 60 &&
-            start.diffNow("minutes").minutes > -60 && (
+          {start.diffNow("minutes").minutes < 30 &&
+            end.diffNow("minutes").minutes > -30 && (
               <a
                 className="text-red-400 hover:text-red-500 text-lg"
                 href={location}
                 title="Join the event"
+                target="_blank"
+                rel="noopener noreferrer"
               >
                 Join &gt;
               </a>
